42e89079c424392ca51b034e31fadb1e0bfd36aa
Екатерина Черняева <dopx@yandex.ru>
2026-01-09 18:25:18 +0800

    Исправила эндпоинты

--- data.py
+++ data.py
@@ -1,11 +1,15 @@
 # API service endpoints:
 BASE_URL = 'https://stellarburgers.education-services.ru/'
 CREATE_USER_URL = f'{BASE_URL}/api/auth/register'
-DELETE_USER_URL = f'{BASE_URL}/api/auth/user'
+DELETE_USER_URL = f'{BASE_URL}/api/auth/user'        # DELETE - удаление пользователя
 LOGIN_USER_URL = f'{BASE_URL}/api/auth/login'
-UPDATE_USER_DATA_URL = f'{BASE_URL}/api/auth/user'
-CREATE_ORDER_URL = f'{BASE_URL}/api/orders'
-GET_ORDER_URL = f'{BASE_URL}/api/orders'
+UPDATE_USER_DATA_URL = f'{BASE_URL}/api/auth/user'   # PATCH - обновление данных о пользователе
+
+# Эндпоинты для работы с заказами
+# Исправляем копипасту - переименовываем второй одинаковый URL
+CREATE_ORDER_URL = f'{BASE_URL}/api/orders'           # POST - создание заказа
+GET_USER_ORDERS_URL = f'{BASE_URL}/api/orders'        # GET - получить заказы конкретного пользователя
+
 GET_INGREDIENTS_URL = f'{BASE_URL}/api/ingredients'
 
 
